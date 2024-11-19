71830334c33954010bb3ceacd8a8343ae6437e5a
Winzlieb <winzlieb@c3d2.de>
2024-11-19 12:37:38 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Flake for dev shell each default system";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python3_11
+          ];
+          LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+        };
+      }
+    );
+}
