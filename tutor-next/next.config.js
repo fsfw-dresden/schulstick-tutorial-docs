229bd4797e2a65a54ab8ec04938886950a49797d
Winzlieb <winzlieb@c3d2.de>
2024-11-23 07:42:01 +0100

    update next js config

--- tutor-next/next.config.js
+++ tutor-next/next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'output',
   distDir: 'export',
   images: {
     unoptimized: true
   },
-  assetPrefix: '.',
   basePath: ''
 }
 
