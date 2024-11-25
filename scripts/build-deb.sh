c01af9081a7a6f5c7a548caf0e34dc1214823f83
Winzlieb <winzlieb@c3d2.de>
2024-11-25 09:14:44 +0100

    default dist dir mount in build deb

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -5,9 +5,7 @@ set -e
 docker build -t schulstick-builder -f docker/Dockerfile.build .
 
 # Run the build process
-docker run --rm -v "$(pwd):/build" -v "$(pwd)/dist:/dist" schulstick-builder bash -c "cd /build && dpkg-buildpackage -us -uc"
+docker run --rm -v "$(pwd):/build" -v "$(pwd)/dist:/build/dist" schulstick-builder bash -c "cd /build && dpkg-buildpackage -us -uc"
 
-# Move the .deb file to a dist directory
-mv ../schulstick_*.deb /dist || echo "No .deb file found to move"
 
 echo "Debian package built successfully! Check the dist/ directory."
