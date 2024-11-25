7eb4acfe3be0bc5a0bb76ee4af75276aba4dc876
Winzlieb <winzlieb@c3d2.de>
2024-11-25 05:51:09 +0100

    refactor: Remove anthropic dependencies from build configuration

--- flake.nix
+++ flake.nix
@@ -19,39 +19,7 @@
           setuptools  # Provides pkg_resources
         ] ++ [ pkgs.qt5.qttools ]; # Adds lrelease and other Qt tools
         
-        commonPropagatedBuildInputs = with pkgs.python3Packages; [
-          (buildPythonPackage rec {
-            pname = "anthropic";
-            version = "0.37.1";
-            format = "pyproject";
-            
-            src = fetchPypi {
-              inherit pname version;
-              sha256 = "sha256-mfaIJleV2qe6klbuaOry8F1TzZnXQX9KDC3CksEG0Ao=";
-            };
-            
-            nativeBuildInputs = with pkgs.python3Packages; [
-              hatchling
-              hatch-fancy-pypi-readme
-            ];
-            
-            propagatedBuildInputs = with pkgs.python3Packages; [
-              httpx
-              pydantic
-              typing-extensions
-              distro
-              boto3
-              google-cloud-core
-              google-api-core
-              google-auth
-              tiktoken
-              tokenizers
-              jiter
-            ];
-            
-            doCheck = false;
-          })
-        ];
+        commonPropagatedBuildInputs = [];
         
       in
       rec {
@@ -119,33 +87,6 @@
           buildInputs = with pkgs; [
             python3
           ] ++ commonBuildInputs ++ [
-            (python3.pkgs.buildPythonPackage rec {
-              pname = "anthropic";
-              version = "0.37.1";
-              format = "pyproject";
-              
-              src = python3.pkgs.fetchPypi {
-                inherit pname version;
-                sha256 = "sha256-mfaIJleV2qe6klbuaOry8F1TzZnXQX9KDC3CksEG0Ao=";
-              };
-              
-              nativeBuildInputs = with python3.pkgs; [
-                hatchling
-                hatch-fancy-pypi-readme
-              ];
-              
-              propagatedBuildInputs = with python3.pkgs; [
-                httpx
-                pydantic
-                typing-extensions
-                distro
-                tiktoken
-                tokenizers
-                jiter
-              ];
-              
-              doCheck = false;
-            })
             python3Packages.pillow
           ];
           LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
