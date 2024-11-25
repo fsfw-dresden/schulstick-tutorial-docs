1ecdce83d090b6e7933cdac61bf4b5947859a728
Winzlieb <winzlieb@c3d2.de>
2024-11-25 12:54:09 +0100

    fix debian docker build process

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -5,7 +5,7 @@ set -e
 docker build -t schulstick-builder -f docker/Dockerfile.build .
 
 # Run the build process
-docker run --rm -v "$(pwd):/build" -v "$(pwd)/dist:/build/dist" schulstick-builder bash -c "cd /build && dpkg-buildpackage -us -uc"
+docker run --rm -v "$(pwd):/build" -v "$(pwd)/dist:/dist" schulstick-builder bash -c "cd /build && dpkg-buildpackage -us -uc && mv ../schulstick* /dist" 
 
 
 echo "Debian package built successfully! Check the dist/ directory."
