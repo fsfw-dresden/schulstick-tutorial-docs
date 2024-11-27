47414f070435bba6127d2cc661080feceec0dd34
Winzlieb <winzlieb@c3d2.de>
2024-11-27 14:06:28 +0100

    refactor: Update UnitMetadata model with optional paths and tutorial URL property

--- src/core/models.py
+++ src/core/models.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, List, Optional
 from dataclass_wizard import YAMLWizard
 
 @dataclass
@@ -13,12 +13,26 @@ class UnitMetadata(YAMLWizard):
     skill_level_per_subject: Dict[str, int]
     markdown_file: str
     preview_image: str
-    unit_path: Path = None
+    unit_url: str = None
+    html_path: str = None
+    unit_path: str = None
+
+    @property
+    def tutorial_url(self) -> Optional[str]:
+        if self.unit_url is None:
+            if self.unit_path is None or self.html_path is None:
+                return None
+            return f"file://{Path(self.unit_path).absolute() / self.html_path}"
+        return self.unit_url
+
+    @property
+    def markdown_path(self) -> Optional[Path]:
+        if self.unit_path is None:
+            return None
+        return Path(self.unit_path) / self.markdown_file
     
     @property
-    def markdown_path(self) -> Path:
-        return self.unit_path / self.markdown_file
-    
-    @property
-    def preview_path(self) -> Path:
-        return self.unit_path / self.preview_image
+    def preview_path(self) -> Optional[Path]:
+        if self.unit_path is None:
+            return None
+        return Path(self.unit_path) / self.preview_image
